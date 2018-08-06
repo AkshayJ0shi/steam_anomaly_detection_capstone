69a0b08ea0de71879764319e0ba1a8b24e588921
Marshall Payne <marshall@Marshalls-MacBook-Pro.local>
2018-08-06 15:38:04 -0500

    added review histogram data to mongo

--- request_review_hist.py
+++ request_review_hist.py
@@ -1,8 +1,9 @@
 import requests
 import ast
 import datetime
+from pymongo import MongoClient
 
-def get_reviews(app_id, app_name):
+def get_reviews(app_id):
     """
     Gets {'date': 1517443200, 'recommendations_up': 36508, 'recommendations_down': 6557} for the app id
     :param app: (dict) {app id: app name}
@@ -12,34 +13,21 @@ def get_reviews(app_id, app_name):
         output = requests.get('https://store.steampowered.com/appreviewhistogram/{}'.format(app_id)) \
          .json()['results']['rollups']
     except:
-        print('No reviews for ' + app_name)
         return None
     for i, item in enumerate(output):
-        item['app'] = app_id
-        item['date'] = datetime.datetime.fromtimestamp(item['date'])
+        item['date'] = str(datetime.datetime.fromtimestamp(item['date']))
         output[i] = item
-    return output
+    output_dict = {'app': str(app_id), 'reviews': output}
+    return output_dict
 
 
-# I'll need to go back and check for non 200's. Seems like it's been a long time since I got a "No reviews for " message
-# I can find the first non 200, then check the app id of the previous entry to get my starting point
-# if __name__ == '__main__':
-#     with open('data/key_to_apps_dict.txt') as f:
-#         app_dict = ast.literal_eval(f.read())
-#
-#     # For now write to file, later write to mongo
-#     with open('data/reviews.txt', 'w') as g:
-#         for app_id, app_name in app_dict.items():
-#             reviews = get_reviews(app_id, app_name)
-#             if reviews:
-#                 g.write(str(reviews))
-
-
-# Code to fix formatting to be able to evaluate with eval():
-# string = re.sub(r'\]\[', '],[', string)
-# string = '[' + string
-# string = string + ']'
-# b = eval(string)
-# for x in range(len(b)):
-#     for y in range(len(b[x])):
-#         b[x][y]['date'] = str(b[x][y]['date'])
+if __name__ == '__main__':
+    with open('data/sourced_id_to_name.txt') as f:
+        app_dict = eval(f.read())
+    client = MongoClient()
+    db = client['steam_capstone']
+    collection = db['reviews']
+    for x in app_dict.keys():
+        r = get_reviews(x)
+        if r:
+            collection.insert(r)
