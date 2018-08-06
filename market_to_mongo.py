bea4d028761085a411d23a10581a0f39fc6f73a5
Marshall Payne <marshall@Marshalls-MacBook-Pro.local>
2018-08-06 10:53:33 -0500

    update data gather script and prepare for merge

--- market_to_mongo.py
+++ market_to_mongo.py
@@ -9,7 +9,9 @@ import sys
 
 def get_market_page(session, app, item):
     response = session.get('https://steamcommunity.com/market/pricehistory/?appid={}&market_hash_name={}'.format(app, url(item)))
-    if response.status_code != 200:
+    if response.status_code == 500:
+        return 'skip'
+    elif response.status_code != 200:
         return None
     else:
         return response
@@ -36,21 +38,39 @@ if __name__ == '__main__':
         item_dict = ast.literal_eval(f.read())
     client = MongoClient()
     db = client['steam_capstone']
+    collection = db['skipped']
     collection = db['market']
     user = wa.WebAuth(os.environ['STEAM_ID'], os.environ["STEAM_PASSWORD"])
     session = user.login()
     i = 0
+    if i%10000 == 0:
+        user = wa.WebAuth(os.environ['STEAM_ID'], os.environ["STEAM_PASSWORD"])
+        session = user.login()
     total = calc_num_items(item_dict)
+    duplicate = True
+    response = True
     for app, items in item_dict.items():
         for item in items.keys():
-            progress(i, total, item)
+            progress(i, total, str(app) + ' ' + item)
+            i += 1
+            if item == 'Kill Covered Sniper Rifle (Minimal Wear)' and app == 440:
+                duplicate = False
+            if duplicate:
+                continue
             response = get_market_page(session, app, item)
             if not response:
                 print('Last app: ' + str(app) + ', Last item: ' + item)
+                break
+            if response == 'skip':
+                db['skipped'].insert({'app': app, 'item': item})
+                continue
+            elif response.json()['success'] == 'false':
+                continue
             mongo_dict = {'item_name':item, 'app': app, 'prices':[]}
             for x in response.json()['prices']:
                 mongo_dict['prices'].append({'date':x[0], 'median_sell_price':x[1], 'quantity':x[2]})
             collection.insert(mongo_dict)
-            i += 1
         if not response:
             break
+
+#244850 Leopard Suit
