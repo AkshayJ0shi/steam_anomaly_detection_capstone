eb704a460b5bf6743fe845dfd23123252ff5d508
Marshall Payne <marshall@Marshalls-MBP.T-mobile.com>
2018-08-18 01:21:43 -0500

    date_util

---
+++ date_util.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import datetime
+import time
+
+
+def string_to_epoch(time_str, format='%d %b %Y'):
+    return time.mktime(datetime.datetime.strptime(time_str, format).timetuple())
+
+def string_to_Timestamp(time_str, format='%d %b %Y'):
+    return epoch_to_Timestamp(string_to_epoch(time_str, format))
+
+def string_to_np(time_str, format='%d %b %Y'):
+    return epoch_to_np(string_to_epoch(time_str, format))
+
+def epoch_to_string(time_float, format='%d %b %Y'):
+    return datetime.datetime.fromtimestamp(time_float).strftime(format)
+
+def epoch_to_Timestamp(time_float):
+    return pd.to_datetime(time_float, unit='s')
+
+def epoch_to_np(time_float):
+    return np.datetime64(datetime.datetime.fromtimestamp(time_float).date())
+
+def timestamp_to_string(timestamp, format='%d %b %Y'):
+    return timestamp.date().strftime(format)
+
+def timestamp_to_epoch(timestamp):
+    pass
+
+def timestamp_to_np(timestamp):
+    pass
+
+def np_to_string(time_np):
+    pass
+
+def np_to_epoch(time_np):
+    pass
+
+def np_to_Timestamp(time_np):
+    pass
+
