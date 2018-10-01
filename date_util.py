82eb695633712fbeaf02fca03cb25fe5865bbe20
Marshall Payne <34942223+mosho-p@users.noreply.github.com>
2018-10-01 16:51:20 -0500

    added more conversions

--- date_util.py
+++ date_util.py
@@ -5,14 +5,18 @@ import time
 import re
 
 
-def date_converter(date, convert_to='string'):
+def date_converter(date, convert_to='string', str_format=False):
     """
     Converts any datetime object into any other (that I've accounted for).
     :param date: any date objects listed below. String must be of the form '15 Jan 2001'
     :param convert_to: Options are 'string', 'float', 'datetime', 'datetime64' (numpy), 'timestamp' (pandas)
+    :param str_format: Specify format if converting to string. Default will be '15 Mar 2001'. Must be specified
+                       to convert from 'string'
     :return: Converted date type object
     """
     # str(type()) returns somethig like "<class 'pandas.Timestamp'>" and I want to extract 'Timestamp'
+    if str_format and convert_to == 'string':
+        return eval(re.findall(r'(\w+)', str(type(date)))[-1].lower() + '_to_string(date, format=str_format)')
     return eval(re.findall(r'(\w+)', str(type(date)))[-1].lower() + '_to_{}(date)'.format(convert_to))
 
 # Need to rename functions, and replace them in other files with the date_converter.
@@ -53,3 +57,29 @@ def datetime64_to_float(time_np):
 
 def datetime64_to_timestamp(time_np):
     return pd.Timestamp(time_np)
+
+def datetime_to_datetime64(date):
+    return np.datetime64(date)
+
+def datetime_to_timestamp(date):
+    return pd.Timestamp(date)
+
+def datetime_to_float(date):
+    return date.timestamp()
+
+def datetime_to_string(date, format='%d %b %Y'):
+    return date.strftime(format)
+
+def string_to_datetime(date, format='%d %b %Y'):
+    return datetime.strptime(date, format)
+
+def float_to_datetime(date):
+    return datetime.fromtimestamp(date)
+
+def datetime64_to_datetime(date):
+    return pd.Timestamp(date).to_pydatetime()
+
+def timestamp_to_datetime(date):
+    return date.to_pydatetime()
+
+
